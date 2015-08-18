a3038bb7ef06948bc01ce19994c223a74f8bbf3e
Matt Smart <mdsmart36@gmail.com>
2015-08-17 21:22:35 -0500

    Initial commit

---
+++ Shapes/Shape.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Shapes
+{
+    public abstract class Shape : IShape
+    {
+        // We need info about colors
+        Color fillColor;
+        Color borderColor;
+
+        public Color FillColor { get; }
+        public Color BorderColor { get; }
+
+        public float Area()
+        {
+            throw new NotImplementedException();
+        }
+
+        public float Perimeter()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
