4c91734f3966a36853acf86d3c1b4201f89fbb1b
Matt Smart <mdsmart36@gmail.com>
2015-09-06 10:19:22 -0500

    populate combobox with shape types Rectangle and Square

--- SharpShapes/MainWindow.xaml.cs
+++ SharpShapes/MainWindow.xaml.cs
@@ -11,7 +11,9 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
+//using System.Windows.Shapes;
+using Shapes;
+using System.Reflection;
 
 namespace SharpShapes
 {
@@ -23,6 +25,14 @@ namespace SharpShapes
         public MainWindow()
         {
             InitializeComponent();
+            SetComboBoxItems();
+        }
+
+        private void SetComboBoxItems()
+        {
+            List<string> stuff = new List<string>();
+            var quadType = typeof(AQuadrilateral);
+            combo1.ItemsSource = Assembly.GetAssembly(typeof(Shapes.AShape)).GetTypes().Where(shapeType => shapeType.IsSubclassOf(typeof(AQuadrilateral)));
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
